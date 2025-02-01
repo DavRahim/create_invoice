93f3084579ead94ea73bdff263b1ad913477bce1
Md Abdur Rahim <mdabdurrahim184275@gmail.com>
2025-02-01 23:55:11 +0600

    one comment add

--- app/utils/db.ts
+++ app/utils/db.ts
@@ -12,3 +12,5 @@ const prisma = globalThis.prismaGlobal ?? prismaClientSingleton()
 export default prisma
 
 if (process.env.NODE_ENV !== "production") globalThis.prismaGlobal = prisma;
+
+// db connect on my server
